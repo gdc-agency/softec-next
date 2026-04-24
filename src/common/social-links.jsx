7f262462668cb5d4fb8d7a7301e88d055379330a
gdc-agency <info.globaldigitalcare@gmail.com>
2026-04-24 11:25:23 +0500

    first commit

--- src/common/social-links.jsx
+++ src/common/social-links.jsx
@@ -49,7 +49,7 @@ export default SocialLinks;
 
 
 const copy_right_text = {
-  copy_right: <>Full Copyright & Design By <Link href="#">@Theme pure</Link> – {new Date().getFullYear()}</>,
+  copy_right: <>Full Copyright & Design By  <a href="https://www.globaldigitalcare.pk/">Global Digital Care</a> – {new Date().getFullYear()}</>,
 }
 const {copy_right}  = copy_right_text
 
